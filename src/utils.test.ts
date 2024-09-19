fc37e4374b43eb2e721eb1cdabcee2118b214e30
Shmuel Leider <shmuel.leider@chabad.org>
2024-09-19 12:54:43 -0400

    add tests

---
+++ src/utils.test.ts
@@ -0,0 +1,10 @@
+import { add } from "./utils.js";
+import { test, expect } from "vitest";
+
+test("add", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test("add multiple", () => {
+	expect(add(4,add(5,6))).toBe(15);
+});
